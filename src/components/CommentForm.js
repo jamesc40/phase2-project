99308ba218eb46e9df7d4e4ac784634bcd094356
James Coffman <jamesc4023@gmail.com>
2022-05-10 11:58:44 -0400

    start of a comment form. Likes are working

---
+++ src/components/CommentForm.js
@@ -0,0 +1,8 @@
+const { useState } from 'react'
+
+export default function CommentForm() {
+        return (
+                <form>
+                        
+                </form>
+        )
--- src/components/RenderTrail.js
+++ src/components/RenderTrail.js
@@ -4,7 +4,7 @@ import { URL } from './App';
 
 export default function RenderTrail() {
          const { id } = useParams();
-         const [trail, setTrail] = useState()
+         const [trail, setTrail] = useState({})
   
          useEffect(() => {
                 fetch(`${URL}/${id}`)
@@ -12,54 +12,43 @@ export default function RenderTrail() {
                 .then(data => setTrail(data))
          }, [id])
         
-        if(!trail) return <h1>Loading...</h1>
 
-        const { name, likes, length, image, description, comments } = trail        
 
         const handleSetTrail = (updatedTrail) => setTrail(updatedTrail)
 
         const handleClick = () => { 
                 setTrail(prev => {
                         return {...trail, likes: prev.likes + 1}
-                        handlePatch()
                 })
         }
 
-       // useEffect(() => {
-       //         fetch(`${URL}/${id}`, {
-       //         method: 'PATCH',
-       //         headers: {
-       //                 'content-type': 'application/json'
-       //         },
-       //         body: JSON.stringify({ likes: 1000000 })
-       //         })
-       //         .then(r => r.json())
-       //         .then(data => console.log(data))
-       // }, [trail.likes])
+        useEffect(() => {
+                handlePatch({ likes: trail.likes });
+       }, [trail.likes])
 
-
-        const handlePatch = () => {
-                console.log(likes)
+        const handlePatch = (update) => {
                 fetch(`${URL}/${id}`, {
                         method: 'PATCH',
-                        header: {
-                        'content-type': 'application/json'
-                }, 
-                        body: JSON.stringify({ likes: likes })
-                        })
-                .then(r => r.json())
-                .then(data => console.log(data))
-        } 
+                        headers: {
+                                'content-type': 'application/json'
+                        },
+                        body: JSON.stringify(update)
+                })
+        }
+
+
+        if(!trail) return <h1>Loading...</h1>
+
+        const { name, likes, length, image, description, comments } = trail        
         
-                  
          return(
-           <div>
-             <h1>{name}</h1>
-             <img src={image} alt={name} className='map'/>
-             <p>Length: {length} miles</p>
-             <p>Description: {description}</p>
-             <button onClick={handleClick}>Likes: {likes}</button>
-             <p>Comments:</p>
-           </div>
+                <div>
+                        <h1>{name}</h1>
+                        <img src={image} alt={name} className='map'/>
+                        <p>Length: {length} miles</p>
+                        <p>Description: {description}</p>
+                        <button onClick={handleClick}>Likes: {likes}</button>
+                        <p>Comments:</p>
+                </div>
          )
 }
