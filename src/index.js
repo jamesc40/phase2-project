cdaa30d441afe14927a35c1099d33542401f5115
James Coffman <jamesc4023@gmail.com>
2022-05-11 14:06:45 -0400

    bootstrap and stuff

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import { BrowserRouter as Router } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
-   
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';  
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Router>
-    <React.StrictMode>
       <App />
-    </React.StrictMode>
   </Router>
 );
