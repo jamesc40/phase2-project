9f9e42b6ee8f743dc535b55866fe34db8f87e6a2
James Coffman <jamesc4023@gmail.com>
2022-05-13 11:38:57 -0400

    home component added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,20 @@
+import NavBarHome from './NavBarHome'
+
 export default function Home() {
-        return (
-          <div>Home</div>
-        )
+  return (
+    <div className='bg d-flex align-items-center justify-content-center'>
+      <div id='intro-parent-div' className='col align-items-center'>
+        <div id='intro-div' className='col text-center'>
+          <p id='intro-p'>
+            Welcome to James and Dan’s trail finder. We’ve compiled a list of
+            popular long-distance hiking trails and relevant details about
+            each one.
+          </p>
+        </div>
+        <div id='NavBarContainer' className='col'>
+          <NavBarHome />
+        </div>
+      </div>
+    </div>
+  );
 }
---
+++ src/components/NavBarHome.js
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+// import AddIcon from '@mui/icons-material/Add';
+// import FormatListBulletedIcon from '@mui/icons-material/FormatListBulleted';
+
+export default function NavBarHome() {
+  return (
+    <nav class='navbar navbar-light bg-light'>
+      <form class='form-inline'>
+        <Link to='/traillist'>
+          <button id='home-btn' class='btn btn-outline-success' type='button'>
+           {/*<FormatListBulletedIcon fontSize='large' className='home-icon' />
+            {'  '} See the list of trails*/}
+            List of Trails
+          </button>
+        </Link>
+        <Link to='/form'>
+          <button id='home-btn' class='btn btn-outline-success' type='button'>
+            {/*<AddIcon fontSize='large' className='home-icon' />
+            {' '} Add a new trail*/}
+            Add a New Trail
+          </button>
+        </Link>
+      </form>
+    </nav>
+  );
+}
