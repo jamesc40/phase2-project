00b72185da83a03b5b51b185e6a0f0a3df9c60d3
James Coffman <jamesc4023@gmail.com>
2022-05-13 11:02:23 -0400

    before adding homescreen

--- src/components/TrailList.js
+++ src/components/TrailList.js
@@ -4,34 +4,34 @@ import Sort from './Sort';
 import Container from 'react-bootstrap/Container';
 
 export default function TrailList({ trails }) {
-   const [renderedTrails, setRender] = useState([]);
-   const [search, setSearch] = useState('');
-   const [sort, setSort] = useState('default');
+  const [renderedTrails, setRender] = useState([]);
+  const [search, setSearch] = useState('');
+  const [sort, setSort] = useState('default');
 
-   const handleSetSort = (e) => setSort(e.target.value)
+  const handleSetSort = (e) => setSort(e.target.value)
 
-   const handleSearch = (e) => setSearch(e.target.value);
-  
-   useEffect(() => {
-      let searchArr = trails.filter(trail => trail.name.toLowerCase().includes(search.toLowerCase()))
-      let row = []
-      let tempArr = sort === 'default' ?  searchArr : [...searchArr].sort((a, b) => b[sort] - a[sort])
-      let mapArr = tempArr.map((trail, index) => (<TrailItem key={trail.id} trail={trail}/>))
-      while (mapArr.length > 0) {
-        row.push(<div className='row' key={mapArr.length}>{mapArr.splice(0, 2)}</div>)
-      } setRender(row)
-   }, [trails, search, sort])
+  const handleSearch = (e) => setSearch(e.target.value);
 
-   return (
-       <Container>
-         <Sort 
-            search={search}
-            sort={sort} 
-            handleSearch={handleSearch}
-            handleSetSort={handleSetSort}  
-         />
-            {renderedTrails}
-      </Container>
-      )
-  }
- 
+  useEffect(() => {
+    let searchArr = trails.filter(trail => trail.name.toLowerCase().includes(search.toLowerCase()))
+    let row = []
+    let tempArr = sort === 'default' ?  searchArr : [...searchArr].sort((a, b) => b[sort] - a[sort])
+    let mapArr = tempArr.map((trail, index) => (<TrailItem key={trail.id} trail={trail}/>))
+    while (mapArr.length > 0) {
+      row.push(<div className='row' key={mapArr.length}>{mapArr.splice(0, 2)}</div>)
+    } setRender(row)
+  }, [trails, search, sort])
+
+  return (
+    <Container>
+      <Sort 
+        search={search}
+        sort={sort} 
+        handleSearch={handleSearch}
+        handleSetSort={handleSetSort}  
+      />
+        {renderedTrails}
+    </Container>
+    )
+    }
+   
