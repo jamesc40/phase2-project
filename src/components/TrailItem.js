e8af537373e6b1325054f26f0469bab71ebde7bc
James Coffman <jamesc4023@gmail.com>
2022-05-13 09:13:38 -0400

    awaiting home component pretty much done

--- src/components/TrailItem.js
+++ src/components/TrailItem.js
@@ -1,19 +1,18 @@
-//import { Link } from 'react-router-dom'
 import Card from "react-bootstrap/Card";
 
 export default function TrailItem({ trail }) {
-    const { id, name, image } = trail 
+  const { id, name, image } = trail 
     
-      return(
-         <div className='col'>
-            <Card style={{ width: '50rem' }}>
-               <Card.Body>
-                  <Card.Title>{ name }</Card.Title>
-                  <Card.Img src={ image } /> 
-                  <Card.Link href={`/traillist/${id}`}>Learn More</Card.Link>
-               </Card.Body>
-            </Card>
-         </div>
-      )
-  }
+  return(
+   <div className='col'>
+      <Card style={{ width: '50rem' }}>
+        <Card.Body>
+          <Card.Title>{ name }</Card.Title>
+          <Card.Img src={ image } /> 
+          <Card.Link href={`/traillist/${id}`}>Learn More</Card.Link>
+        </Card.Body>
+      </Card>
+   </div>
+  )
+}
   
